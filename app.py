a5a24869c7c842006b9a4cbd4bc67e768c61e421
zpwenz <zachary.p.wenz@live.com>
2020-05-03 06:49:34 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,125 @@
+import os
+from flask import Flask, render_template, redirect, request
+import pandas as pd
+import pickle
+from sklearn import model_selection, preprocessing, linear_model, naive_bayes, metrics, svm
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn import decomposition, ensemble
+import xgboost, numpy, textblob, string
+from keras.preprocessing import text, sequence
+from keras import layers, models, optimizers
+import spacy
+nlp = spacy.load("en_core_web_sm")
+import en_core_web_sm
+from absolang import absolutist, absolutist_index
+import re, string
+import inflect
+from nltk import word_tokenize, sent_tokenize
+import unicodedata
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import nltk
+nltk.download('punkt')
+from nltk.tokenize import word_tokenize
+from collections import Counter
+import json
+
+afinn_wl_url = ('https://raw.githubusercontent.com'
+                '/fnielsen/afinn/master/afinn/data/AFINN-111.txt')
+sentiments = pd.read_csv(afinn_wl_url,
+                          header=None, # no column names
+                          sep='\t',  # tab sepeated
+                          names=['term', 'value']) #new column names
+
+
+
+# Use pickle to load in the pre-trained model.
+with open('dummy_model_2.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+with open('vectorizer.pkl', 'rb') as v:
+    vectorizer = pickle.load(v)
+
+def personal_pronouns(text1):
+    pronouns = ["i", "me", "my", "mine", "myself"]
+    count = 0
+    words = text1.split ()
+    for w in words:
+        if w.lower() in pronouns:
+             count += 1
+    return(count)
+
+def to_lowercase(words):
+    """Convert all characters to lowercase from list of tokenized words"""
+    new_words = []
+    for word in words:
+        new_word = word.lower()
+        new_words.append(new_word)
+    return new_words
+
+import contractions
+from contraction_list import CONTRACTION_MAP
+
+def replace_contractions(text):
+    """Replace contractions in string of text"""
+    return contractions.fix(text)
+
+def tfs_dict(text):
+    a_list = nltk.tokenize.sent_tokenize(text)
+    sall = []
+    for sentence in a_list:
+        term = "I" #term we want to search for
+        words = sentence.split() #split the sentence into individual words
+        if term in words:
+            replace_contractions(sentence)
+            doc=nlp(sentence)
+            sall.append(doc)
+    verbs = []
+    for sentence in sall:
+        s_tokens = [token.lower_ for token in sentence] #if (token.pos_ == "VERB")]
+        verbs.append(s_tokens)
+    verb_list = [item for sublist in verbs for item in sublist]
+    c = Counter(verb_list)
+    words_f = [([w, c.get(w, 0)]) for w in sentiments["term"].tolist()]
+    s_words = list(filter(lambda x: x[1] != 0, words_f))
+    tf = pd.DataFrame(s_words, columns = ["term", "frequency"])
+    tfv = tf.merge(sentiments)
+    colors = []
+    for value in tfv['value']:
+        if value > 0:
+            colors.append("positive")
+        else:
+            colors.append("negative")
+    tfv["group"] = colors
+    return tfv.to_dict('records')
+
+app = Flask(__name__)
+
+# Main home page
+@app.route("/", methods=['GET', 'POST'])
+def whatever():
+    if request.method == 'GET':
+        return render_template('index.html')
+
+
+    if request.method == 'POST':
+    
+        text = request.form['text']
+        textc = replace_contractions(text)
+        text1 = [textc]
+        text2 = to_lowercase(text1)
+        text3 = str(text2)
+        inputvector =  vectorizer.transform(text1)
+        prediction = model.predict_proba(inputvector)[0][1]
+        absolutist = absolutist_index(text3)
+        sentiments = SentimentIntensityAnalyzer().polarity_scores(text3).get('compound')
+        pronoun = personal_pronouns(text3)
+        return render_template('index.html',
+                                     original_input={'Text':text3},
+                                     words = textc,
+                                     result=prediction, absolutist = absolutist, sentiment = sentiments, pronouns = pronoun,
+                                     data = json.dumps(tfs_dict(text))
+                                     )
+        
+
+if __name__ == "__main__":
+    app.run(debug=True)
